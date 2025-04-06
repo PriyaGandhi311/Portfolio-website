56427ab9cb8fede4067dacc0f82d8013104de41d
PriyaGandhi <priyagandhi4688@gmail.com>
2025-04-06 18:19:45 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,7 +41,7 @@ const Hero = () => {
         />
       </div>
 
-      <div className="flex flex-col justify-center relative my-15 z-10 pt-6">
+      <div className="flex flex-col justify-center items-center relative my-15 z-10 pt-6">
         
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <div className="flex items-center justify-center">
