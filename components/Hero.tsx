774df40e531c0d306c52a68c7b8547fa71dbf835
PriyaGandhi <priyagandhi4688@gmail.com>
2025-04-06 15:49:34 -0400

    Update profile picture

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,10 +15,7 @@ const handleDownload = () => {
 const Hero = () => {
   return (
     <div id="about" className="pb-20 pt-36 h-screen">
-      {/**
-       *  UI: Spotlights
-       *  Link: https://ui.aceternity.com/components/spotlight
-       */}
+     
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -31,18 +28,14 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      {/**
-       *  UI: grid
-       *  change bg color to bg-black-100 and reduce grid color from
-       *  0.2 to 0.03
-       */}
+      
       <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
         {/* Radial gradient for the container to give a faded look */}
         <div
-          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
+          
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
@@ -50,15 +43,15 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10 pt-8">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          {/* <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
-          </p> */}
+         
 
-          {/**
-           *  Link: https://ui.aceternity.com/components/text-generate-effect
-           *
-           *  change md:text-6xl, add more responsive code
-           */}
+          <div className="flex items-center justify-center mb-4">
+            <img
+              src="/Priya_Gandhi.png"
+              alt="Priya Gandhi"
+              className="rounded-full w-32 h-32 md:w-44 md:h-44 lg:w-52 lg:h-52"
+            />
+          </div>
           <TextGenerateEffect
             words="Hi, I'm Priya Gandhi!"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
